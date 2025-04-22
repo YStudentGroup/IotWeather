42d23795ea308c2cd1cb76132237118ce1c2dc7e
theo.fratia <theofratia22@gmail.com>
2025-04-22 10:24:22 +0200

    add some data

--- SimulateData.py
+++ SimulateData.py
@@ -2,7 +2,7 @@ import random
 from datetime import datetime
 
 def get_weather():
-    conditions = ['ensoleillé', 'nuageux', 'pluvieux', 'orageux', 'neigeux', 'brumeux']
+    conditions = ['ensoleillé', 'nuageux', 'pluvieux', 'orageux', 'neigeux',]
     temperatures = {
         'été': (20, 35),
         'hiver': (-5, 5),
@@ -20,19 +20,25 @@ def get_weather():
     else:
         saison = 'automne'
 
+    vent_min, vent_max = (0, 80)
+    vent = round(random.uniform(vent_min, vent_max), 1)
+    humidite = round(random.uniform(0, 100), 1)
     temp_min, temp_max = temperatures[saison]
     temperature = round(random.uniform(temp_min, temp_max), 1)
+    ressentie = round(random.uniform(temperature - 3, temperature + 3), 1)
     
     condition = random.choice(conditions)
 
     return {
         'temperature': f"{temperature}°C",
+        'ressentie': f"{ressentie}°C",
         'condition': condition,
         'saison': saison,
+        'vent': f"{vent} km/h",
+        'humidite': f"{humidite}%",
         'date': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
-
 if __name__ == "__main__":
-    meteo = get_weather()
-    print("Météo simulée :", meteo)
+    weather_data = get_weather()
+    print("Données météorologiques simulées : ", weather_data)
