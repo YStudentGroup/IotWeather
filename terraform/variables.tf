319bcc31f4c34f2cb2e35de8d3fdd39bad3f8df8
Seb Portable <sebastien.giguet@ynov.com>
2025-05-19 14:39:12 +0200

    Fix-Pipe

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "admin_username" {
 }
 
 variable "ssh_public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+  default = "C:\Users\User\.ssh\id_rsa.pub"
 }
 
 variable "resource_group_name" {
