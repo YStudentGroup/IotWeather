7ee6c03829aa0a88268f81a1b47f444c774f93b5
Seb Portable <sebastien.giguet@ynov.com>
2025-05-19 15:29:19 +0200

    Fix-Pipe

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+Configuration des providers requis pour le projet,
+terraform {
+  required_providers { 
+    azurerm    = {
+      source   = "hashicorp/azurerm"
+      version  = "~> 3.113"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name   = var.backend_rg_name
+    storage_account_name  = var.backend_account_name 
+    container_name        = var.backend_container_name 
+    key                   = "terraform.tfstate"
+    access_key            = var.backend_access_key
+  }
+}
+
+Configuration du provider Azure,
+provider "azurerm" {
+  features {}
+}
