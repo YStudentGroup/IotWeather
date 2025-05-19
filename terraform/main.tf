74c031d65c051fce666793c41f5feac1a82cde67
Seb Portable <sebastien.giguet@ynov.com>
2025-05-19 11:14:42 +0200

    Initial commit infra + pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features = {}
+}
+
+# Variables (à adapter selon ton besoin)
+variable "resource_group_name" {
+  default = "Seb-RG"
+}
+
+variable "location" {
+  default = "France Central"
+}
+
+variable "admin_username" {
+  default = "fabio.maratea@ynov.com"
+}
+
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+# Lecture de la clé SSH publique
+data "local_file" "ssh_key" {
+  filename = var.ssh_public_key_path
+}
+
+# Groupe de ressources
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Réseau virtuel
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-ci-cd"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Sous-réseau
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-ci-cd-Seb"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# IP publique
+resource "azurerm_public_ip" "main" {
+  name                = "public-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# Interface réseau
+resource "azurerm_network_interface" "main" {
+  name                = "nic-ci-cd"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# Machine virtuelle Linux
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "vm-ci-cd"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = data.local_file.ssh_key.content
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "osdisk-ci-cd"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
+# Output : IP publique
+output "public_ip" {
+  value = azurerm_public_ip.main.ip_address
+  description = "Adresse IP publique de la VM"
+}
