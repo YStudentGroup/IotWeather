f92519d62b82ad9c5345edfccb08cefb97940360
Seb Portable <sebastien.giguet@ynov.com>
2025-05-19 16:19:00 +0200

    Fix-Pipe

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,5 +11,27 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default = "France Central"
+  description = "La région Azure où les ressources seront déployées"
+  default     = "France Central"
+}
+
+variable "backend_container_name" {
+  description = "Nom du conteneur de stockage pour le backend Terraform"
+}
+
+variable "backend_rg_name" {
+  description = "Nom du groupe de ressources pour le backend Terraform"
+}
+
+variable "backend_account_name" {
+  description = "Nom du compte de stockage pour le backend Terraform"
+}
+
+
+variable "backend_access_key" {
+  description = "Clé d'accès pour le compte de stockage du backend Terraform"
+}
+
+variable "admin_password" {
+  description = "Clé d'accès pour le compte de stockage du backend Terraform"
 }
